1da108d8534d0ac7409d12d56001a26fa8ce7d17
xvbnm48 <muhamadfarizwisnuprananda@gmail.com>
2021-10-16 23:15:15 +0700

    database golang

---
+++ database_test.go
@@ -0,0 +1,17 @@
+package belajar_golang_database
+
+import (
+	"database/sql"
+	"testing"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func TestOpenConnection(t *testing.T){
+	db, err := sql.Open("mysql" , "root:@tcp(localhost:3306)/belajar_golang_database")
+	if err != nil{
+		panic(err)
+	}
+
+	defer db.Close()
+}
