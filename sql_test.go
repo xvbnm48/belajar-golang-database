5ba97a678c15c1301928c5d73e88f4f6660831f6
xvbnm48 <muhamadfarizwisnuprananda@gmail.com>
2021-10-16 23:48:42 +0700

    membuat connection db secara terpisah

---
+++ sql_test.go
@@ -0,0 +1,23 @@
+package belajar_golang_database
+
+import (
+	"context"
+	"fmt"
+	"testing"
+)
+
+func TestExecSql(t *testing.T) {
+	db := GetConnection()
+	defer db.Close()
+
+	ctx := context.Background()
+
+	script := "INSERT INTO customer(id, name) VALUES('sakura','sakura endo')"
+	_, err := db.ExecContext(ctx, script)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("success insert new customer")
+}
